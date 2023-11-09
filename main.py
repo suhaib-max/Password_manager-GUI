2a855b6d82b9a64bd1ee9d9a73971e59bd1ec13f
suhaib <suhaibvbm@gmail.com>
2023-11-09 07:08:00 +0530

    image placed on canvas

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password manager")
+window.config(padx=20,pady=20)
+
+canvas = Canvas(width=200,height=200)
+image_loc = PhotoImage(file="logo.png")
+canvas.create_image(100,100,image =image_loc)
+canvas.pack()
 
 
 window.mainloop()
